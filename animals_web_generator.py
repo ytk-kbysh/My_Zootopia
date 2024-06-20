465590510b8dff483e0aee656e62d1ed062ddf83
ytk-kbysh <kobayashi4registrations@utakata.jetzt>
2024-06-20 12:05:31 +0200

    html is updated with json data thru the  python codes

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -5,14 +5,44 @@ def load_data(file_path):
   with open(file_path, "r") as handle:
     return json.load(handle)
   
+def load_html(file_path):
+  """ Loads an html file """
+  with open(file_path, "r") as data:
+    return data.read()
+  
+def rewrite_html(file_path, new_data):
+  """ Loads an html file """
+  with open(file_path, "w") as data:
+    return data.write(new_data)
 
-  animals_data = load_data('animals_data.json')
+animals_data = load_data('animals_data.json')
+html_data = load_html('animals_template.html')
 
-for item in load_data('animals_data.json'):
-    print(f"Name: {item['name']}")
-    print(f"Diet: {item['characteristics']['diet']}")
-    print(f"Location: {item['locations'][0]}")
+# i = 1
+# for line in html_data:
+#     print(f"line {i} : {line}")
+#     i += 1
+
+# for item in animals_data:
+#     print(f"Name: {item['name']}")
+#     print(f"Diet: {item['characteristics']['diet']}")
+#     print(f"Location: {item['locations'][0]}")
+#     try:
+#         print(f"Type: {item['characteristics']['type']}")
+#     except:
+#         pass
+
+output = ''  # define an empty string
+for item in animals_data:
+    # append information to each string
+    output += f"Name: {item['name']}\n"
+    output += f"Diet: {item['characteristics']['diet']}\n"
+    output += f"Location: {item['locations'][0]}\n"
     try:
-        print(f"Type: {item['characteristics']['type']}")
+        output += f"Type: {item['characteristics']['type']}\n"
     except:
         pass
+
+re_html = html_data.replace("__REPLACE_ANIMALS_INFO__", output)
+
+rewrite_html('animals_template.html', re_html)
